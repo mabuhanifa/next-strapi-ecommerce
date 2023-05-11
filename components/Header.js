0bb13148f1f074fa20d8976459dc7b1a307bbc9b
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-11 10:54:14 +0600

    Wrapper

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
 export default function Header() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(false);
   return (
-    <div>Header</div>
-  )
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <div></div>
+    </header>
+  );
 }
---
+++ components/Wrapper.js
@@ -0,0 +1,6 @@
+
+export default function Wrapper({children , className}) {
+  return (
+    <div>Wrapper</div>
+  )
+}
