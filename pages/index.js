df49842ba2f67c66908fb951d8c78c8d3d8242c4
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-11 10:45:04 +0600

    header and footer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+
 export default function Home() {
   return (
     <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quo, praesentium numquam molestiae deserunt cumque obcaecati placeat nemo, dolore repellendus aut accusamus temporibus illum officia dignissimos id. Voluptates, reiciendis mollitia?
+      
     </div>
   );
 }
