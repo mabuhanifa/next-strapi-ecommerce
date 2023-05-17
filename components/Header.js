5f7f3d6f62c85d9fbaf5a702849cc3f03f564c8e
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-17 18:11:23 +0600

    liked button

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { BsCart } from "react-icons/bs";
+import { IoMdHeartEmpty } from "react-icons/io";
 import Menu from "./Menu";
 import Wrapper from "./Wrapper";
 
@@ -30,8 +31,27 @@ export default function Header() {
             className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center
            hover:bg-black/[0.05] cursor-pointer relative"
           >
-            <BsCart />
-            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-4 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]"/>
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full
+               bg-red-600 absolute top-1 left-4 md:left-7 text-white text-[10px] 
+               md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]"
+            >
+              51
+            </div>
+          </div>
+          <div
+            className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center
+           hover:bg-black/[0.05] cursor-pointer relative"
+          >
+            <BsCart className="text-[15px] md:text-[20px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full
+               bg-red-600 absolute top-1 left-4 md:left-7 text-white text-[10px] 
+               md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]"
+            >
+              5
+            </div>
           </div>
         </div>
       </Wrapper>
