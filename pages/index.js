c4394484c5171b4e0d5fdbe58933d1803ca4bed1
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-18 21:33:41 +0600

    HeroBanner

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
+import HeroBanner from "@/components/HeroBanner";
 
 export default function Home() {
   return (
-    <div className="bg-white h-[2000px]">
-      
+    <div className="bg-white h-[2000px] text-gray-700">
+      <HeroBanner/>
     </div>
   );
 }
