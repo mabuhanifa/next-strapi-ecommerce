a308ec8f4096a42c19b4843c30759712667d5373
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-23 17:51:58 +0600

    RelatedProducts updated

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
-    <div className="text-gray-700">
+    <div>
       <HeroBanner />
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
-          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight ">
             Cushioning for Your Miles
           </div>
           <div className="text-md md:text-xl">
